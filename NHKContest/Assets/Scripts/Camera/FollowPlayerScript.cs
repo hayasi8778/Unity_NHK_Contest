ce8ebc00861bcfc8e7951388d78d5ed4261e281d
WIN11OHS30193\FL6Au <ohs30193ohs.hal.ac.jp>
2025-04-28 11:43:23 +0900

    カメラの追従形式変えたのとオブジェクト切り替えた時にカメラががくってならないように

--- NHKContest/Assets/Scripts/Camera/FollowPlayerScript.cs
+++ NHKContest/Assets/Scripts/Camera/FollowPlayerScript.cs
@@ -8,9 +8,11 @@ public class FollowPlayerScript : MonoBehaviour
     public Transform target;
     public Vector3 offset;
 
+    private bool isFollowing = true; //カメラの追従フラグ
+
     void LateUpdate()
     {
-        if (target != null)//ターゲットが設定されてるなら
+        if (isFollowing && target != null)
         {
             transform.position = target.position + offset;
         }
@@ -19,6 +21,19 @@ public class FollowPlayerScript : MonoBehaviour
     public void SetTarget(Transform newTarget)//オブジェクト切り替えても継承するための関数
     {
         target = newTarget;
+        PauseFollowing(1.5f);//1.5秒間カメラの追従止める
+    }
+
+    public void PauseFollowing(float pauseDuration)//指定秒数間カメラ追従止める関数
+    {
+        StartCoroutine(PauseCoroutine(pauseDuration));
+    }
+
+    private System.Collections.IEnumerator PauseCoroutine(float pauseDuration)
+    {
+        isFollowing = false;
+        yield return new WaitForSeconds(pauseDuration);//指定秒数間待ちが発生する処理
+        isFollowing = true;
     }
 
 }
