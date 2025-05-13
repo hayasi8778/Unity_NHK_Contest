5d23fafd53be414b34f621edf732a57cc4d5964c
gitabc0 <gitabc000@gmail.com>
2025-05-13 21:23:47 +0900

    ステージ後半つくった

--- NHKContest/Assets/Scripts/DestroyMesh.cs
+++ NHKContest/Assets/Scripts/DestroyMesh.cs
@@ -4,9 +4,6 @@ public class DestroyMesh : MonoBehaviour
 {
     private void Awake()
     {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
-        Destroy(meshFilter);
-        Destroy(meshRenderer);
+        Destroy(gameObject);
     }
 }
--- NHKContest/Assets/Scripts/SliderTimeCounter.cs
+++ NHKContest/Assets/Scripts/SliderTimeCounter.cs
@@ -112,6 +112,25 @@ public class SliderTimeCounter : MonoBehaviour
             timeCounter = 0f;
         }
 
+        if (slider.value > 0)
+        {
+            if (currentObjects != null)
+            {
+                for (int i = 0; i < currentObjects.Length; i++)
+                {
+                    GameObject obj = currentObjects[i];
+                    if (obj == null) continue;
+
+                    var timeObj = obj.GetComponent<TimeSliderObject>();
+                    if (timeObj != null)
+                    {
+                        timeObj.UpdatePositionHistory(slider.value);
+                    }
+                }
+            }
+        }
+
+
         // 履歴が一周したら動きの差分チェック
         if (/* historyIndex == 0 && */changeCooldownTimer <= 0f)//判断基準一旦切る
         {
