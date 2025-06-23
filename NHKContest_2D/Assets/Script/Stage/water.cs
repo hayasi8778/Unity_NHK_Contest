571a7db75a9652e34fe38310759f0d1684924d1d
ryoya-nakatani <ohs30558@ohs.hal.ac.jp>
2025-06-23 09:54:46 +0900

    kari

--- NHKContest_2D/Assets/Script/Stage/water.cs
+++ NHKContest_2D/Assets/Script/Stage/water.cs
@@ -8,7 +8,11 @@ public class WaterScript : MonoBehaviour
 
     // このオブジェクトのRendererとColliderの参照を保持
     private Renderer objRenderer;
-    private Collider objCollider;
+
+    [Header("水流から2つ目のブロックか")]
+    public bool second_Block = false;
+    [Header("重力反転時に描画するか")]
+    public bool Flip = false;
 
     // 水流の一つ手前のブロックの参照を保持
     [SerializeField] private GameObject previousBlock;
@@ -20,32 +24,37 @@ public class WaterScript : MonoBehaviour
     private float wasDeltaTime;
 
     // 押し戻す力
-    public float forceStrength = 500f; 
+    public float forceStrength = 500f;
 
-    void Start() {
+    void Start()
+    {
         // 同じオブジェクトに付いているRendererとColliderを取得
         objRenderer = GetComponent<Renderer>();
-        objCollider = GetComponent<Collider>();
     }
 
     void Update()
     {
-        if (!isBlockInWater) {
+        if (!isBlockInWater)
+        {
             // 水流の一つ手前のブロックのRendererが有効なら
-            if (previousBlock.GetComponent<Renderer>().enabled) {
+            if (previousBlock.GetComponent<Renderer>().enabled)
+            {
                 // Rendererを有効にする
                 if (objRenderer != null) objRenderer.enabled = true;
-            } else {
+            }
+            else
+            {
                 // Rendererを無効にする
                 if (objRenderer != null) objRenderer.enabled = false;
             }
         }
     }
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
         // 接触したオブジェクトがブロックタグを持っているか確認
-        if (other.gameObject.CompareTag(blockTag)) {
+        if (other.gameObject.CompareTag(blockTag))
+        {
             // Rendererを無効にする
             if (objRenderer != null) objRenderer.enabled = false;
 
@@ -57,24 +66,27 @@ public class WaterScript : MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         // 接触したオブジェクトがプレイヤータグを持っているか確認
-        if (other.CompareTag(playerTag) && !isBlockInWater) {
+        if (other.CompareTag(playerTag) && !isBlockInWater)
+        {
             Rigidbody rb = other.GetComponent<Rigidbody>();
-            if (rb != null) {
+            if (rb != null)
+            {
                 // プレイヤーを押し戻す
                 if (objRenderer.enabled)//レンダラーが有効なら(水オブジェクトとしてシーンに登場しているなら)
                 {
                     Vector3 pushDirection = (other.transform.position - transform.position).normalized;
                     rb.AddForce(pushDirection * forceStrength);
                 }
-                
+
             }
         }
     }
 
-    void OnTriggerExit(Collider other) 
+    void OnTriggerExit(Collider other)
     {
         // 接触が終了したオブジェクトがブロックタグを持っているか確認
-        if (other.gameObject.CompareTag(blockTag)) {
+        if (other.gameObject.CompareTag(blockTag))
+        {
             // Rendererを有効にする
             if (objRenderer != null) objRenderer.enabled = true;
 
