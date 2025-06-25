f440e2208e15a02e88aecd4bb511e85418616194
ryoya-nakatani <ohs30558@ohs.hal.ac.jp>
2025-06-25 17:47:32 +0900

    waterの重力反転対応
    あと、中の処理が３D用から変えてなかったので２D用に修正しました。

--- NHKContest_2D/Assets/Script/Stage/water.cs
+++ NHKContest_2D/Assets/Script/Stage/water.cs
@@ -14,6 +14,12 @@ public class WaterScript : MonoBehaviour
     [Header("重力反転時に描画するか")]
     public bool Flip = false;
 
+    [Header("重力反転ギミックがある場合、PlayerObjectの参照を設定")]
+    public GameObject PlayerObject;
+    public bool isGravity = true;
+    public bool wasGravity = true;
+
+    [Header("水流の一つ手前のブロックの参照を設定")]
     // 水流の一つ手前のブロックの参照を保持
     [SerializeField] private GameObject previousBlock;
 
@@ -30,27 +36,51 @@ public class WaterScript : MonoBehaviour
     {
         // 同じオブジェクトに付いているRendererとColliderを取得
         objRenderer = GetComponent<Renderer>();
+
+        if (!Flip) {
+            // Rendererを有効にする
+            if (objRenderer != null) objRenderer.enabled = true;
+        } else {
+            // Rendererを無効にする
+            if (objRenderer != null) objRenderer.enabled = false;
+        }
     }
 
     void Update()
     {
-        if (!isBlockInWater)
+        // 重力の反転状況を取得
+        if (PlayerObject != null) isGravity = PlayerObject.GetComponent<PlayerMove>().GetGravityFlag();
+
+        if (!isBlockInWater && !second_Block)   // 水源から二番目のブロック以外
         {
-            // 水流の一つ手前のブロックのRendererが有効なら
-            if (previousBlock.GetComponent<Renderer>().enabled)
+            if (previousBlock != null)
             {
-                // Rendererを有効にする
-                if (objRenderer != null) objRenderer.enabled = true;
+                // 水流の一つ手前のブロックのRendererが有効なら
+                if (previousBlock.GetComponent<Renderer>().enabled)
+                {
+                    // Rendererを有効にする
+                    if (objRenderer != null) objRenderer.enabled = true;
+                }
+                else
+                {
+                    // Rendererを無効にする
+                    if (objRenderer != null) objRenderer.enabled = false;
+                }
             }
-            else
+            
+        }
+        else if (!isBlockInWater && second_Block)   // 水源から二番目のブロック
+        {
+            if (isGravity != wasGravity) // 重力状況が変化した際
             {
-                // Rendererを無効にする
-                if (objRenderer != null) objRenderer.enabled = false;
+                objRenderer.enabled = !objRenderer.enabled; // 反転する
             }
         }
+
+        wasGravity = isGravity; // 前回の重力反転状態を保存
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         // 接触したオブジェクトがブロックタグを持っているか確認
         if (other.gameObject.CompareTag(blockTag))
@@ -63,12 +93,12 @@ public class WaterScript : MonoBehaviour
         }
     }
 
-    void OnTriggerStay(Collider other)
+    void OnTriggerStay2D(Collider2D other)
     {
         // 接触したオブジェクトがプレイヤータグを持っているか確認
         if (other.CompareTag(playerTag) && !isBlockInWater)
         {
-            Rigidbody rb = other.GetComponent<Rigidbody>();
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
                 // プレイヤーを押し戻す
@@ -82,7 +112,7 @@ public class WaterScript : MonoBehaviour
         }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit2D(Collider2D other)
     {
         // 接触が終了したオブジェクトがブロックタグを持っているか確認
         if (other.gameObject.CompareTag(blockTag))
